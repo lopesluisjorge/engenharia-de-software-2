6b4b7e53450ef446f724913645b240e6a955b872
Jorge Luis L. Amorim <lopesluisjorge@hotmail.com>
2019-11-02 00:20:10 -0300

    Corrigindo Identação

--- src/main/java/br/edu/ifma/livraria/servico/EmprestimoService.java
+++ src/main/java/br/edu/ifma/livraria/servico/EmprestimoService.java
@@ -8,12 +8,11 @@ import br.edu.ifma.livraria.modelo.Usuario;
 
 public class EmprestimoService {
 
-	public Emprestimo emprestaLivro(Usuario usuario, Livro livro) {
+    public Emprestimo emprestaLivro(Usuario usuario, Livro livro) {
         Emprestimo emprestimo = new Emprestimo(usuario, livro, LocalDate.now());
         livro.setEmprestado(true);
         livro.adicionaEmprestimo(emprestimo);
         return emprestimo;
-	}
+    }
 
-    
 }
