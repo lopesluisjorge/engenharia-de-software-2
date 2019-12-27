0bcbe8a8e93dd24e52456684dda42bf91d6efa2c
Jorge Luis L. Amorim <lopesluisjorge@hotmail.com>
2019-12-27 12:04:47 -0300

    iniciando primeiro projeto da etapa 3

---
+++ transportadora/src/test/java/br/edu/ifma/es2/transportadora/TransportadoraApplicationTests.java
@@ -0,0 +1,14 @@
+package br.edu.ifma.es2.transportadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportadoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
