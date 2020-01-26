f3af5a0d804af2b066760768b943327d5499cfa1
Jorge Luis L. Amorim <lopesluisjorge@hotmail.com>
2020-01-26 10:58:59 -0300

    Movendo projeto para raiz

--- livraria/src/main/java/br/edu/ifma/livraria/servico/EnviadorDeEmail.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifma.livraria.servico;
-
-import br.edu.ifma.livraria.modelo.Usuario;
-
-public interface EnviadorDeEmail {
-
-    Email notificar(final Usuario usuario, TipoNotificacao tipo);
-
-}
